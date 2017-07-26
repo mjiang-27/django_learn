aa7790981aa4ea86e3956e7c05d41079aa987b0c
mjiang-27 <JMM2015@163.com>
2017-07-26 02:18:45 -0700

    Add new project, combining models with forms. ToDo: add method for deleting book objects.

---
+++ form_advanced_combine_models/app/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+         return self.name
